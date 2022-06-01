3cf6f751b7d99c1bab51a14beb88670624febadd
CarlG96 <carl.gardiner1@hotmail.com>
2022-06-01 16:20:51 +0000

    Initial commit, add main function and start of name input

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    """Main function"""
+    print('Hello World!')
+    print('Please enter your name.')
+    print('Name should be between 5 and 15 characters long.')
+    print("Name can't include numbers.")
+    name_prototype = input('Input your name:\n')
+    name = name_prototype.strip()
+    print(name)
+    validate_name(name)
+    
+
+def validate_name(name):
+    [str(character) for character in name]
+    print(f'{name}')
+    
+
+main()
