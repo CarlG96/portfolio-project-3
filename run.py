4cc2fc1f27710964661ab19d8d913ba16b8c5348
CarlG96 <carl.gardiner1@hotmail.com>
2022-06-02 14:18:49 +0000

    feat: Add introduction function and start of get_name function

--- run.py
+++ run.py
@@ -1,13 +1,29 @@
+def introduction():
+    print('Welcome to Star Traveller!')
+    print('You are a captain of the Star Republic Navy.')
+    print('You have been tasked with delivering a superweapon from Sector A to Sector E.')
+    print('This will allow the Star Republic to defeat the Robo-Empire.')
+    print('You will face many perils on your way there, but the Star Republic is relying on you!')
+    print('\n\n\n')
+
+
+def get_name():
+    name = (input('What is your name, captain? ')).strip()
+    return name
+
+
 def main():
     """Main function"""
-    print('Hello World!')
-    print('Please enter your name.')
-    print('Name should be between 5 and 15 characters long.')
-    print("Name can't include numbers.")
-    name_prototype = input('Input your name:\n')
-    name = name_prototype.strip()
-    print(name)
-    validate_name(name)
+    introduction()
+    print(f'Hello, {get_name()}.')
+    # print('Hello World!')
+    # print('Please enter your name.')
+    # print('Name should be between 5 and 15 characters long.')
+    # print("Name can't include numbers.")
+    # name_prototype = input('Input your name:\n')
+    # name = name_prototype.strip()
+    # print(name)
+    # validate_name(name)
     
 
 def validate_name(name):
